3d0f3d602742d6c062c186c7420407b7ce85de45
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-29 13:18:30 +0530

    shimmer Implementation

--- src/pages/ItemList.js
+++ src/pages/ItemList.js
@@ -11,7 +11,7 @@
 //   const [regionName, setRegionName] = useState('');
 //   const navigate = useNavigate();
 //   const { token, setToken } = useContext(MyContext);
-//   const [headerFooterColor, setHeaderFooterColor] = useState('#125B57'); 
+//   const [headerFooterColor, setHeaderFooterColor] = useState('#125B57');
 //   const [num_of_species, setNumOfSpecies] = useState([]);
 //   const [allItems, setAllItems] = useState([]);
 //   const [filterItem, setFilterItem] = useState([]);
@@ -49,7 +49,7 @@
 //           const response = await axios.post('https://farmersforforests.org/admin/acc/appdata/specieslistinfo', {
 //             region_id: regionId,
 //           });
-          
+
 //           setNumOfSpecies(response.data.species_list_name);
 //           setAllItems(response.data.species_list_data);
 //           setFilterItem(response.data.species_list_data); // Show all items initially
@@ -98,7 +98,7 @@
 //           >
 //             All
 //           </button>
-          
+
 //           {num_of_species && num_of_species.map(option => (
 //             <button
 //               key={option.species_type_id}
@@ -114,7 +114,7 @@
 //             </button>
 //           ))}
 //         </div>
-        
+
 //         {loading ? (
 //           <p>Loading...</p>
 //         ) : (
@@ -122,7 +122,7 @@
 //             {filterItem.length > 0 ? (
 //               filterItem.map((item, index) => (
 //                 <Link to={`/item/${regionId}/${item.species_id}`} key={index} className="w-full">
-  
+
 //                 <li key={index} className="flex  py-2 border-b border-gray-300">
 //                   <img
 //                     src= 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg'
@@ -166,49 +166,51 @@
 
 // export default ItemList;
 
-
-import React, { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
-import { Link, useParams, useNavigate } from 'react-router-dom';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import { MyContext } from '../context/accountProvider';
-import LoadingAnimation from '../components/LoadingAnimation'; // Import the loading animation component
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { MyContext } from "../context/accountProvider";
+import LoadingAnimation from "../components/LoadingAnimation"; // Import the loading animation component
+import { ShimmerCategoryItems, ShimmerDiv } from "shimmer-effects-react";
 
 const ItemList = () => {
   const { regionId } = useParams();
   const [loading, setLoading] = useState(true);
-  const [regionName, setRegionName] = useState('');
+  const [regionName, setRegionName] = useState("");
   const navigate = useNavigate();
   const { token, setToken } = useContext(MyContext);
-  const [headerFooterColor, setHeaderFooterColor] = useState('#125B57');
+  const [headerFooterColor, setHeaderFooterColor] = useState("#125B57");
   const [num_of_species, setNumOfSpecies] = useState([]);
   const [allItems, setAllItems] = useState([]);
   const [filterItem, setFilterItem] = useState([]);
-  const [filteredValue, setFilterValue] = useState('All'); // 'All' selected by default
+  const [filteredValue, setFilterValue] = useState("All"); // 'All' selected by default
+  const [isImageLoaded, setIsImageLoaded] = useState(false); // State to manage image load
 
-  const fixedColor = '#125B57';
+  const fixedColor = "#125B57";
 
   useEffect(() => {
-    const storedColor = sessionStorage.getItem('headerFooterColor') || fixedColor;
-    sessionStorage.setItem('headerFooterColor', storedColor);
+    const storedColor =
+      sessionStorage.getItem("headerFooterColor") || fixedColor;
+    sessionStorage.setItem("headerFooterColor", storedColor);
     setHeaderFooterColor(storedColor);
   }, []);
 
   useEffect(() => {
     const checkToken = () => {
-      const storedToken = localStorage.getItem('token');
+      const storedToken = localStorage.getItem("token");
       if (!storedToken) {
         setToken(null);
-        navigate('/');
+        navigate("/");
       }
     };
 
     checkToken();
-    window.addEventListener('storage', checkToken);
+    window.addEventListener("storage", checkToken);
 
     return () => {
-      window.removeEventListener('storage', checkToken);
+      window.removeEventListener("storage", checkToken);
     };
   }, [navigate, setToken]);
 
@@ -216,9 +218,12 @@ const ItemList = () => {
     const fetchData = async () => {
       try {
         if (regionId) {
-          const response = await axios.post('https://farmersforforests.org/admin/acc/appdata/specieslistinfo', {
-            region_id: regionId,
-          });
+          const response = await axios.post(
+            "https://farmersforforests.org/admin/acc/appdata/specieslistinfo",
+            {
+              region_id: regionId,
+            }
+          );
           console.log(response);
           setNumOfSpecies(response.data.species_list_name);
           setAllItems(response.data.species_list_data);
@@ -228,12 +233,12 @@ const ItemList = () => {
             setRegionName(response.data.species_list_data[0].region_name);
           }
         } else {
-          console.error('Region ID not found');
+          console.error("Region ID not found");
         }
         // Delay setting loading to false by 1 second
         setTimeout(() => setLoading(false), 1000);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setLoading(false);
       }
     };
@@ -244,10 +249,12 @@ const ItemList = () => {
   const handleButtonClick = (value) => {
     setFilterValue(value);
 
-    if (value === 'All') {
+    if (value === "All") {
       setFilterItem(allItems); // Show all items when "All" is selected
     } else {
-      const filteredData = allItems.filter(item => item.species_type === value);
+      const filteredData = allItems.filter(
+        (item) => item.species_type === value
+      );
       setFilterItem(filteredData);
     }
   };
@@ -256,6 +263,10 @@ const ItemList = () => {
     return <LoadingAnimation />; // Show the loading animation
   }
 
+  const handleImageLoad = () => {
+    setIsImageLoaded(true); // Set image loaded state to true when image is fully loaded
+  };
+
   return (
     <div className="mx-[20px]">
       <Header title={regionName} color={headerFooterColor} />
@@ -263,57 +274,80 @@ const ItemList = () => {
         <div className="flex justify-start gap-5 py-2 overflow-x-scroll">
           {/* Add the "All" button */}
           <button
-            onClick={() => handleButtonClick('All')}
+            onClick={() => handleButtonClick("All")}
             style={{
               ...styles.button,
-              backgroundColor: filteredValue === 'All' ? headerFooterColor : 'transparent',
-              color: filteredValue === 'All' ? '#FFFFFF' : headerFooterColor,
-              borderColor: filteredValue === 'All' ? 'transparent' : headerFooterColor,
+              backgroundColor:
+                filteredValue === "All" ? headerFooterColor : "transparent",
+              color: filteredValue === "All" ? "#FFFFFF" : headerFooterColor,
+              borderColor:
+                filteredValue === "All" ? "transparent" : headerFooterColor,
             }}
           >
             All
           </button>
-          
-          {num_of_species && num_of_species.map(option => (
-            <button
-              key={option.species_type_id}
-              onClick={() => handleButtonClick(option.species_type)}
-              style={{
-                ...styles.button,
-                backgroundColor: option.species_type === filteredValue ? headerFooterColor : 'transparent',
-                color: option.species_type === filteredValue ? '#FFFFFF' : headerFooterColor,
-                borderColor: option.species_type === filteredValue ? 'transparent' : headerFooterColor,
-              }}
-            >
-              {option.species_type}
-            </button>
-          ))}
-        </div>
-        
-        <ul className="">
-        {filterItem.length > 0 ? (
-  filterItem.map((item, index) => (
-    <Link to={`/item/${regionId}/${item.species_id}`} key={index} className="w-full">
-      <li className="flex mb-2 mt-2 border-b border-gray-300 shadow-lg rounded-md">
-        <img
- src={`${process.env.PUBLIC_URL}/assests/${item.species_image_name}`}
-          alt={item.species_common_name}
-          className="w-32 h-32 object-cover mr-4 rounded-md"
-        />
-        <div className="flex flex-col justify-between py-5">
-         <div>
-         <p className="text-sm text-black">{item.species_type}</p>
-          <h3 className="text-[16px] font-semibold">{item.species_common_name}</h3>
-         </div>
-          <p className="text-xs text-gray-500"></p>
-        </div>
-      </li>
-    </Link>
-  ))
-) : (
-  <p>No items found</p>
-)}
 
+          {num_of_species &&
+            num_of_species.map((option) => (
+              <button
+                key={option.species_type_id}
+                onClick={() => handleButtonClick(option.species_type)}
+                style={{
+                  ...styles.button,
+                  backgroundColor:
+                    option.species_type === filteredValue
+                      ? headerFooterColor
+                      : "transparent",
+                  color:
+                    option.species_type === filteredValue
+                      ? "#FFFFFF"
+                      : headerFooterColor,
+                  borderColor:
+                    option.species_type === filteredValue
+                      ? "transparent"
+                      : headerFooterColor,
+                }}
+              >
+                {option.species_type}
+              </button>
+            ))}
+        </div>
+
+        <ul className="">
+          {filterItem.length > 0 ? (
+            filterItem.map((item, index) => (
+              
+              <Link
+                to={`/item/${regionId}/${item.species_id}`}
+                key={index}
+                className="w-full"
+              >
+              {!isImageLoaded && <ShimmerCategoryItems mode="light" />
+}
+                <li className="flex mb-2 mt-2 border-b gap-[16px] border-gray-300 shadow-lg rounded-md">
+               
+                  <img
+                    src={`${process.env.PUBLIC_URL}/assests/${item.species_image_name}`}
+                    alt={item.species_common_name}
+                    className={`w-32 h-32 object-cover rounded-md ${isImageLoaded ? "":"hidden"}`}
+                    onLoad={handleImageLoad}
+                   
+                  />
+                  <div className={`flex flex-col justify-between py-5 ${isImageLoaded?"":"hidden"}`}>
+                    <div>
+                      <p className="text-sm text-black">{item.species_type}</p>
+                      <h3 className="text-[16px] font-semibold">
+                        {item.species_common_name}
+                      </h3>
+                    </div>
+                    <p className="text-xs text-gray-500"></p>
+                  </div>
+                </li>
+              </Link>
+            ))
+          ) : (
+            <p>No items found</p>
+          )}
         </ul>
       </div>
 
@@ -327,13 +361,13 @@ const ItemList = () => {
 // Styles object
 const styles = {
   button: {
-    padding: '10px 20px',
-    fontSize: '12px',
-    cursor: 'pointer',
-    border: '1px solid',
-    borderRadius: '10px',
-    transition: 'background-color 0.3s, color 0.3s, border-color 0.3s',
-    textAlign: 'center',
+    padding: "10px 20px",
+    fontSize: "12px",
+    cursor: "pointer",
+    border: "1px solid",
+    borderRadius: "10px",
+    transition: "background-color 0.3s, color 0.3s, border-color 0.3s",
+    textAlign: "center",
   },
 };
 
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,3 +1,124 @@
+// import React, { useEffect, useContext, useState } from "react";
+// import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+// import Grid from "@mui/material/Grid";
+// import Button from "@mui/material/Button";
+// import { styled } from "@mui/material/styles";
+// import { useNavigate } from "react-router-dom";
+// import "../styles/HomePage.css";
+// import Footer from "../components/Footer";
+// import Header from "../components/Header";
+// import { useSession } from "../components/SessionContext";
+// import { MyContext } from "../context/accountProvider";
+// import HomepageHeader from "../components/HomepageHeader";
+// import { Image, Shimmer } from "react-shimmer";
+
+// // import { useNavigate } from 'react-router-dom';
+
+// const CustomButton = styled(Button)(({ bgcolor }) => ({
+//   borderRadius: "16px",
+//   width: "100%",
+//   height: "80px",
+//   fontSize: "16px",
+//   backgroundColor: bgcolor,
+// }));
+
+// const HomePage = () => {
+//   const navigate = useNavigate();
+//   // const { setHeaderFooterColor } = useSession();
+//   const { token, setToken } = useContext(MyContext);
+//   const [headerFooterColor, setHeaderFooterColor] = useState("#125B57"); // Sta
+
+//   const fixedColor = "#125B57";
+//   useEffect(() => {
+//     const storedColor =
+//       sessionStorage.getItem("headerFooterColor") || fixedColor;
+//     sessionStorage.setItem("headerFooterColor", storedColor);
+//     setHeaderFooterColor(storedColor);
+//   }, []);
+
+//   useEffect(() => {
+//     const checkToken = () => {
+//       const storedToken = localStorage.getItem("token");
+//       if (!storedToken) {
+//         setToken(null);
+//         navigate("/");
+//       }
+//     };
+
+//     // Run on initial load
+//     checkToken();
+
+//     // Run when localStorage changes
+//     window.addEventListener("storage", checkToken);
+
+//     return () => {
+//       window.removeEventListener("storage", checkToken);
+//     };
+//   }, [navigate, setToken]);
+
+//   const buttons = [
+//     { label: "Western Ghats", color: "#BA744A", id: "er105" },
+//     { label: "Wetland", color: "#4293B8", id: "er106" },
+//     { label: "Central Forest", color: "#E05555", id: "er101" },
+//     { label: "Mandala Garden", color: "#125B57", id: "er104" },
+//     { label: "Dry and Arid Region", color: "#F4910F", id: "er102" },
+//     { label: "Grasslands", color: "#86A34D", id: "er103" },
+//   ];
+
+//   const handleButtonClick = (btn) => {
+//     if(btn.id!=="er103"){
+//     sessionStorage.setItem("headerFooterColor", btn.color);
+//     setHeaderFooterColor(btn.color);
+//     navigate(`/region/${btn.id}`);
+//     }
+//   };
+
+//   return (
+//     <div className="home-page">
+//       <HomepageHeader title="Home Page" color={fixedColor} />
+
+//       <div className=" w-full !box-border  flex  flex-wrap px-[12px] mt-9">
+//         {buttons.map((btn, index) => (
+//           <div className="w-1/2 px-2 py-2 relative" key={index}>
+//             <button
+//               style={{ backgroundColor: btn.color }} // Use inline styles for dynamic colors
+//               className="w-full h-24 text-[14px]  text-white rounded-[10px]"
+//               onClick={() => handleButtonClick(btn)}
+//               disabled={btn.id==="er103"}
+//             >
+//               {btn.label}
+//             </button>
+//             {/* <span className="absolute right-5 bottom-4  z-[100]"><ArrowForwardIcon className="!text-[18px] text-white"/></span> */}
+//           </div>
+//         ))}
+//       </div>
+
+    
+
+//       <h4 className="text-left px-[20px] pt-2 font-bold text-[#125B57]">Map</h4>
+
+    
+//       <section className="px-[20px] py-4">
+//         <img
+//           src={`${process.env.PUBLIC_URL}/assests/Homepage.jpg`}
+//           alt="homepage"
+//           className="w-full rounded-lg"
+//           // onError={handleImageError}
+//         />
+//       </section>
+
+//       <div className="mt-16">
+//         <Footer color={fixedColor} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
+
+
+
+
 import React, { useEffect, useContext, useState } from "react";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Grid from "@mui/material/Grid";
@@ -10,9 +131,8 @@ import Header from "../components/Header";
 import { useSession } from "../components/SessionContext";
 import { MyContext } from "../context/accountProvider";
 import HomepageHeader from "../components/HomepageHeader";
-import { Image, Shimmer } from "react-shimmer";
 
-// import { useNavigate } from 'react-router-dom';
+import { ShimmerDiv } from "shimmer-effects-react";
 
 const CustomButton = styled(Button)(({ bgcolor }) => ({
   borderRadius: "16px",
@@ -24,14 +144,13 @@ const CustomButton = styled(Button)(({ bgcolor }) => ({
 
 const HomePage = () => {
   const navigate = useNavigate();
-  // const { setHeaderFooterColor } = useSession();
   const { token, setToken } = useContext(MyContext);
-  const [headerFooterColor, setHeaderFooterColor] = useState("#125B57"); // Sta
-
+  const [headerFooterColor, setHeaderFooterColor] = useState("#125B57");
+  const [isImageLoaded, setIsImageLoaded] = useState(false); // State to manage image load
   const fixedColor = "#125B57";
+
   useEffect(() => {
-    const storedColor =
-      sessionStorage.getItem("headerFooterColor") || fixedColor;
+    const storedColor = sessionStorage.getItem("headerFooterColor") || fixedColor;
     sessionStorage.setItem("headerFooterColor", storedColor);
     setHeaderFooterColor(storedColor);
   }, []);
@@ -45,10 +164,7 @@ const HomePage = () => {
       }
     };
 
-    // Run on initial load
     checkToken();
-
-    // Run when localStorage changes
     window.addEventListener("storage", checkToken);
 
     return () => {
@@ -66,46 +182,48 @@ const HomePage = () => {
   ];
 
   const handleButtonClick = (btn) => {
-    if(btn.id!=="er103"){
-    sessionStorage.setItem("headerFooterColor", btn.color);
-    setHeaderFooterColor(btn.color);
-    navigate(`/region/${btn.id}`);
+    if (btn.id !== "er103") {
+      sessionStorage.setItem("headerFooterColor", btn.color);
+      setHeaderFooterColor(btn.color);
+      navigate(`/region/${btn.id}`);
     }
+  };
+
+  const handleImageLoad = () => {
+    setIsImageLoaded(true); // Set image loaded state to true when image is fully loaded
   };
 
   return (
     <div className="home-page">
       <HomepageHeader title="Home Page" color={fixedColor} />
 
-      <div className=" w-full !box-border  flex  flex-wrap px-[12px] mt-9">
+      <div className="w-full !box-border flex flex-wrap px-[12px] mt-9">
         {buttons.map((btn, index) => (
           <div className="w-1/2 px-2 py-2 relative" key={index}>
             <button
-              style={{ backgroundColor: btn.color }} // Use inline styles for dynamic colors
-              className="w-full h-24 text-[14px]  text-white rounded-[10px]"
+              style={{ backgroundColor: btn.color }}
+              className="w-full h-24 text-[14px] text-white rounded-[10px]"
               onClick={() => handleButtonClick(btn)}
-              disabled={btn.id==="er103"}
+              disabled={btn.id === "er103"}
             >
               {btn.label}
             </button>
-            {/* <span className="absolute right-5 bottom-4  z-[100]"><ArrowForwardIcon className="!text-[18px] text-white"/></span> */}
           </div>
         ))}
       </div>
 
-      {/* <section className="pb-[4px]">
-      <img src={`${process.env.PUBLIC_URL}/homepagemap.png`} alt='Map' className="w-[100%] p-[20px]"/>
-      </section> */}
-
       <h4 className="text-left px-[20px] pt-2 font-bold text-[#125B57]">Map</h4>
 
-    
       <section className="px-[20px] py-4">
+        {/* Show shimmer while image is loading */}
+        {!isImageLoaded && <ShimmerDiv mode="light" height={348} width="100%" className="!rounded-lg"/>}
+
+        {/* Actual image */}
         <img
           src={`${process.env.PUBLIC_URL}/assests/Homepage.jpg`}
           alt="homepage"
-          className="w-full rounded-lg"
-          // onError={handleImageError}
+          className={`w-full rounded-lg ${isImageLoaded ? "block" : "hidden"}`}
+          onLoad={handleImageLoad} // Event handler to trigger when image is loaded
         />
       </section>
 
