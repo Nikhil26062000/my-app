d83a61e0530f3f73aa67f030d1229cf96e94052e
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-09-21 13:12:18 +0530

    changes the api url to local

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-// export const api_url = "http://192.168.1.101";
-export const api_url = "https://farmersforforests.org";
+export const api_url = "http://192.168.1.101";
+// export const api_url = "https://farmersforforests.org";
