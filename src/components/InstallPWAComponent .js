1bc38a69615216ca81aecf57fbdffe45a6716b1b
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-09-27 15:13:24 +0530

    pop up added

--- src/components/InstallPWAComponent .js
+++ src/components/InstallPWAComponent .js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import DownloadIcon from '@mui/icons-material/Download';
 
 const InstallPWAComponent = () => {
   const [deferredPrompt, setDeferredPrompt] = useState(null);
 
   useEffect(() => {
     // Listen for the beforeinstallprompt event
+    console.log("Working......");
     window.addEventListener('beforeinstallprompt', (event) => {
+        console.log('beforeinstallprompt event fired');
       event.preventDefault(); // Prevent the mini-infobar from appearing
       setDeferredPrompt(event); // Save the event for triggering later
     });
@@ -28,7 +31,9 @@ const InstallPWAComponent = () => {
   return (
     <div>
       {deferredPrompt && (
-        <button onClick={handleInstallClick}>Install App</button>
+        <div className="w-full flex justify-center">
+        <button onClick={handleInstallClick} className="px-[25px] py-[10px] ro bg-[#125b57] rounded-[20px] text-[12px] tracking-[1.5px] leading-[19.36px] font-[700] text-white mt-5 " style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}>INSTALL APP <DownloadIcon/></button>
+        </div>
       )}
     </div>
   );
